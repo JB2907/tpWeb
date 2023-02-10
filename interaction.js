cb07b2e65a02d244035b02a67ed1ef428d00f115
Jean-Baptiste <jean-baptiste.bougaud@laposte.net>
2023-02-10 08:57:21 +0100

    first 5 questions

--- interaction.js
+++ interaction.js
@@ -2,11 +2,42 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
-
+  // Définir ici les attributs de la 'classe'
+  this.posInitX = 0;
+  this.posInitY = 0;
+  this.posFinalX = 0;
+  this.posFinalY = 0;
+  this.isOnClick = false;
+	
 	// Developper les 3 fonctions gérant les événements
+  DnD.prototype.onClick = function(evt) {
+    var res = getMousePosition(canvas, evt);
+    this.posInitX = res.x;
+    this.posInitY = res.y;
+    this.isOnClick = true;
+    console.log(evt.clientX, evt.clientY);
+  }
+  
+  DnD.prototype.onDrag = function(evt) {
+    if (this.isOnClick) {
+      var res = getMousePosition(canvas, evt);
+      this.posFinalX = res.x;
+      this.posFinalY = res.y;
+    }
+    console.log(evt.clientX, evt.clientY);
+  }
+  
+  DnD.prototype.onUnclick = function(evt) {
+    if (this.isOnClick) {
+      this.isOnClick = false;
+    }
+    console.log(evt.clientX, evt.clientY);
+  }
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.onClick, false);
+  canvas.addEventListener('mousemove', this.onDrag, false);
+  canvas.addEventListener('mouseup', this.onUnclick, false);
 };
 
 
